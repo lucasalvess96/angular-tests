551f2b14fc53ab0cf5d847984a38b8bdb0e8c12a
lucasalvess96 <lucasalves.57@hotmail.com>
2023-02-02 21:18:52 -0300

    feat: adicionando mais testes

--- src/app/heroes/services/hero-service.service.spec.ts
+++ src/app/heroes/services/hero-service.service.spec.ts
@@ -5,7 +5,7 @@ import { Heroes } from '../models/heroes';
 import { HeroServiceService } from './hero-service.service';
 
 // TEST WITH SPY
-describe('HeroServiceService', () => {
+describe('HeroServiceService with spy', () => {
   let httpClientSpy: jasmine.SpyObj<HttpClient>;
   let service: HeroServiceService;
 
@@ -14,11 +14,11 @@ describe('HeroServiceService', () => {
     service = new HeroServiceService(httpClientSpy);
   });
 
-  it('should be created', () => {
+  it('should be created with spy', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should return expected heroes (HttpClient called once)', (done: DoneFn) => {
+  it('should return expected heroes (HttpClient called once) with spy', (done: DoneFn) => {
     const expectedHeroes: Heroes[] = [
       {
         id: 1,
@@ -36,11 +36,41 @@ describe('HeroServiceService', () => {
     });
     expect(httpClientSpy.get.calls.count()).withContext('one call').toBe(1);
   });
+
+  it('#getHeroes should return value from observable with spy', (done: DoneFn) => {
+    const expectedHeroes: Heroes[] = [
+      {
+        id: 1,
+        name: 'lucas',
+        active: true,
+      },
+    ];
+    httpClientSpy.get.and.returnValue(of(expectedHeroes));
+    service.getHeroes().subscribe((value) => {
+      expect(value).toBe(expectedHeroes);
+      done();
+    });
+  });
+
+  it('#getValue should return real value', () => {
+    const expectedHeroes: Heroes[] = [
+      {
+        id: 1,
+        name: 'json-server',
+        active: true,
+      },
+    ];
+    httpClientSpy.get.and.returnValue(of(expectedHeroes));
+    service.getHeroes().subscribe((value) => {
+      expect(value).toBe(expectedHeroes);
+    });
+  });
 });
 
 // TEST WITH TESTBED WITHOUT SPY
 describe('HeroServiceService with TestBed', () => {
   let serviceTestBed: HeroServiceService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
@@ -61,6 +91,20 @@ describe('HeroServiceService with TestBed', () => {
         active: true,
       },
     ];
+    serviceTestBed
+      .getHeroes()
+      .subscribe((value) => expect(value).toEqual(expectedHeroes));
+  }));
+
+  it('should use HeroServiceService', waitForAsync(() => {
+    const expectedHeroes: Heroes[] = [
+      {
+        id: 1,
+        name: 'json-server',
+        active: true,
+      },
+    ];
+    serviceTestBed = TestBed.inject(HeroServiceService);
     serviceTestBed
       .getHeroes()
       .subscribe((value) => expect(value).toEqual(expectedHeroes));
